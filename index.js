e5b1e76e2b3ded28d0f74d646e4b58e3832c4393
Emily Platzer <emilyplatzer@gmail.com>
2017-09-07 21:29:39 -0600

    export object literal with values of functions... methods! :tada:

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-module.exports = (inventory, searchItem) => {
-  for (fabric in inventory) {
-    if (fabric === searchItem) {
-      if (inventory[fabric] === 0) { return "currently out of stock" }
-      return `${inventory[fabric]} yards`
+module.exports = {
+  inStock: (inventory, searchItem) => {
+    for (fabric in inventory) {
+      if (fabric === searchItem) {
+        if (inventory[fabric] === 0) { return "currently out of stock" }
+        return `${inventory[fabric]} yards`
+      }
     }
+    return "oh shit"
+  },
+  calculateCost: (nameOfFabric, yardage) => {
+    return `please pretend something great happened with ${yardage} yards of ${nameOfFabric}`
   }
-  return "oh shit"
 }
 
 // in the parent directory of this repo:
 // $ node
-// > const callinIt = require('./index.js')
-// > callinIt(
+// > const ftc = require('./index.js')
+// > ftc.inStock(
 // ...
 // ... {
 // ...   'Anna Maria Horner something fabric': 12,
@@ -20,6 +25,8 @@ module.exports = (inventory, searchItem) => {
 // ... },
 // ... 'Anna Maria Horner something fabric'
 // ... )
+//
+// > ftc.calculateCost('something fabric', 10)
 
 
 // can also test for:
